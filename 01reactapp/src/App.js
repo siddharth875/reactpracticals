5ccbcb3762ca56b6df40ec29a8fc986378bb3977
Siddharth Chauhan <siddharthchauhan931@gmail.com>
2023-09-23 14:40:52 +0530

    Create React App using 'CRA'

---
+++ 01reactapp/src/App.js
@@ -0,0 +1,26 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          This is the Heading
+        </h1>
+        <p>
+          This is simple para for testign purpose
+        </p>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open Google
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
