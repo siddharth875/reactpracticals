b266d4db7db33df3361933c165e2f759aeb27b36
Siddharth Chauhan <siddharthchauhan931@gmail.com>
2023-09-24 10:47:41 +0530

    Use React.createElement and jsx object to use in Render Method

--- 01reactapp/src/index.js
+++ 01reactapp/src/index.js
@@ -2,9 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const reactElement = React.createElement(
+  'a',
+  {
+    href : "https://www.google.com",
+    target : "_blank"
+  },
+  "Click me to Visit Google"
+)
+
+const anotherElement = (
+<a href='https://amazon.in' target='_blank'>Click me to visit Amazon.in</a>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <>
     <App />
+    <hr/>
+    {reactElement} <br/>
+    {anotherElement}
+    </>
+    
   </React.StrictMode>
 );
